fc98ec71e5fe7a34ffe68559baf958cece8f14e2
abhishek98s <abhishekshakya98.com@gmail.com>
2023-04-11 16:20:46 +0545

    added the input field

--- client/src/Components/LandingPage/Feed/Post/index.tsx
+++ client/src/Components/LandingPage/Feed/Post/index.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './post.module.scss'
-import { Attach_File, Camera, Image, Location } from '../../../../../public/SVG'
+import Image from 'next/image'
+import { Attach_File, Camera, ImageIcon, Location } from '../../../../../public/SVG'
 
 const Post = () => {
+  const [imgs, setImg] = useState();
+
+
+
+  function file(e: any) {
+    let file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    if (file.type && !file.type.startsWith('image/')) {
+      console.log('File is not an image.', file.type, file);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.addEventListener('load', (e: any) => {
+      // console.log(e.target.result);
+      setImg(e.target.result)
+    });
+    reader.readAsDataURL(file);
+  }
+
+
+
+
+
   return (
 
     <form className={`${styles.post_box} +  mt-[24px] rounded-[10px] p-[24px]`}>
@@ -22,27 +49,51 @@ const Post = () => {
       </article>
 
 
+
+      { imgs && 
+
+      <div className='relative'>
+
+        <Image src={imgs!} className={`w-[70px] h-[70px] mt-[16px] rounded-[15px] object-cover opacity-90`} width={500} height={500} alt='asd' />
+        
+      </div>
+      }
+
+
+
       <section className={`${styles.bottom} + flex justify-between items-center mt-[16px]`}>
 
         <article className={`flex gap-[16px] items-center`}>
 
-          <div className={`${styles.svg} + w-[24px] h-[24px]`}>
-            <Camera />
+          <div className={`${styles.svg} + cursor-not-allowed w-[24px] h-[24px]`}>
+            <div className='cursor-not-allowed'>
+              <Camera />
+            </div>
+          </div>
+
+          <div className={`${styles.svg} + cursor-pointer w-[24px] h-[24px]`}>
+            <label htmlFor='thumbImg' className='cursor-pointer'>
+              <ImageIcon />
+            </label>
           </div>
 
           <div className={`${styles.svg} + w-[24px] h-[24px]`}>
-            <Image />
+            <div className='cursor-not-allowed'>
+
+              <Attach_File />
+            </div>
           </div>
 
           <div className={`${styles.svg} + w-[24px] h-[24px]`}>
-            <Attach_File />
-          </div>
+            <div className='cursor-not-allowed'>
 
-          <div className={`${styles.svg} + w-[24px] h-[24px]`}>
-            <Location />
+              <Location />
+            </div>
           </div>
 
         </article>
+
+        <input className={`hidden`} type='file' id='thumbImg' onChange={file} accept=".jpg, .jpeg, .png" />
 
 
 
