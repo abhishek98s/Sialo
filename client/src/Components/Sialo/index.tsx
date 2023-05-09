5dcfad93820bd02111ff34fbbfb6df0af1a5388b
abhishek <abhishek_mi@yonefu.info>
2023-05-09 15:11:45 +0545

    some changes

--- client/src/Components/Sialo/index.tsx
+++ client/src/Components/Sialo/index.tsx
@@ -5,7 +5,7 @@ const Sialo = () => {
   return (
     <section className={`${styles.sialo_box} + relative z-[9999999] top-0 w-[100%] flex justify-center items-center h-[100vh]`}>
 
-      <h3 className='${styles.logo} + text-[100px] font-bold animate-bounce'>Sialo</h3>
+      <h3 className='${styles.logo} + text-[100px] font-bold animate-bounce animate-pulse'>Sialo</h3>
     </section>
   )
 }
