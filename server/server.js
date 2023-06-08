c666b05d9ada6133c8b835696d40834ae4e20084
abhishek <abhishek_mi@yonefu.info>
2023-06-08 18:39:18 +0545

    some change

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ app.use(cors({
     origin: '*'
 }));
 
+app.use(express.json({ limit: '50mb' }));
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Origin');
