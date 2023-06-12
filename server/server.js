46499b61682987194ffaff05e8d8ae430d336442
abhishek <abhishek_mi@yonefu.info>
2023-06-12 09:47:23 +0545

    fixed i guess

--- server/server.js
+++ server/server.js
@@ -8,13 +8,14 @@ dotenv.config();
 
 
 const app = express();
-app.use(bodyParser.json({ limit: '10mb' }));
+
 app.use(express.json())
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 // app.use(cors({
 //     origin: '*'
 // }));
-
-app.use(express.json({ limit: '500mb' }));
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'https://sialo.vercel.app');
