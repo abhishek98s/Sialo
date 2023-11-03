3138db8468e95ec7a061b5c67750ce4838165c81
abhishek98s <abhishekshakya98.com@gmail.com>
2023-11-03 12:13:43 +0545

    added freemode

--- client/src/Components/LandingPage/Feed/Stories/Story/index.tsx
+++ client/src/Components/LandingPage/Feed/Stories/Story/index.tsx
@@ -6,14 +6,10 @@ import Image from 'next/image'
 
 const Story = ({ ...story }) => {
     return (
-        <section className={`${styles.storys} + mx-[5px] w-[max-content] flex-shrink-0 snap-start`}>
-
-            <div className={`${styles.story} + w-[100px] p-[4px] bg-slate-200 h-[150px] rounded-[8px] grid place-items-center`}>
-                <div className={`w-[100%] h-[100%] rounded-[8px] brightness-[90%] overflow-hidden`}>
-                    <Image src={story.img} alt="story" width={400} height={500} />
-                </div>
+        <section className={`${styles.storys} + mx-[1px] w-[100px] h-[150px] brightness-[90%] flex-shrink-0 snap-start`}>
+            <div className={`w-[100%] h-[100%] rounded-[8px] overflow-hidden`}>
+                <Image src={story.img} alt="story" width={400} height={500} />
             </div>
-
         </section>
     )
 }
--- client/src/Components/LandingPage/Feed/Stories/index.tsx
+++ client/src/Components/LandingPage/Feed/Stories/index.tsx
@@ -6,87 +6,66 @@ import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default class Stories extends Component {
-    render() {
-        const settings = {
-            dots: false,
-            arrows: false,
+import SwiperCore, { Navigation, Pagination, FreeMode, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
 
-            swipeToSlide: true,
-            infinite: false,
-            speed: 500,
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            variableWidth: true,
-            centerPadding: "4px",
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        infinite: false,
-                        dots: false
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        initialSlide: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 6,
-                        slidesToScroll: 1,
-                        infinite: false,
-                        initialSlide: 5
-                    }
-                }
-            ]
-        };
-
-        const story = [
-            { id: 1, img: "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80" },
-            { id: 2, img: "https://images.unsplash.com/photo-1542290425-b5d02738ef3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhbmRvbSUyMGNsaWNrc3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" },
-            { id: 3, img: "https://plus.unsplash.com/premium_photo-1661700093968-b538c5a9f539?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" },
-            { id: 4, img: "https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
-            { id: 5, img: "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
-            { id: 6, img: "https://plus.unsplash.com/premium_photo-1663051160162-e004fc97891e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
-            { id: 7, img: "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80" },
-            { id: 8, img: "https://images.unsplash.com/photo-1542290425-b5d02738ef3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhbmRvbSUyMGNsaWNrc3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" },
-            { id: 9, img: "https://plus.unsplash.com/premium_photo-1661700093968-b538c5a9f539?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" },
-            { id: 10, img: "https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
-            { id: 11, img: "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
-            { id: 12, img: "https://plus.unsplash.com/premium_photo-1663051160162-e004fc97891e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
-            { id: 13, img: "https://images.unsplash.com/photo-1519750783826-e2420f4d687f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
-        ]
-
-        return (
-            <section className={`w-[100%] mt-[56px]`}>
-
-                <div className={`max-lg:hidden`}>
-
-                    <Slider {...settings}>
-
-                        {story.map((story: any, index: number) => (
-                            <Story key={index} {...story} />
-                        ))}
-
-                    </Slider>
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 
-                </div>
+const Stories = () => {
+    const story = [
+        { id: 1, img: "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80" },
+        { id: 2, img: "https://images.unsplash.com/photo-1542290425-b5d02738ef3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhbmRvbSUyMGNsaWNrc3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" },
+        { id: 3, img: "https://plus.unsplash.com/premium_photo-1661700093968-b538c5a9f539?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" },
+        { id: 4, img: "https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
+        { id: 5, img: "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
+        { id: 6, img: "https://plus.unsplash.com/premium_photo-1663051160162-e004fc97891e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
+        { id: 7, img: "https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80" },
+        { id: 8, img: "https://images.unsplash.com/photo-1542290425-b5d02738ef3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHJhbmRvbSUyMGNsaWNrc3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" },
+        { id: 9, img: "https://plus.unsplash.com/premium_photo-1661700093968-b538c5a9f539?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cmFuZG9tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" },
+        { id: 10, img: "https://images.unsplash.com/photo-1550686041-366ad85a1355?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
+        { id: 11, img: "https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
+        { id: 12, img: "https://plus.unsplash.com/premium_photo-1663051160162-e004fc97891e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
+        { id: 13, img: "https://images.unsplash.com/photo-1519750783826-e2420f4d687f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" },
+    ]
 
-                <section className={`${styles.mobile_story} hidden max-lg:flex overflow-scroll snap-x snap-mandatory`}>
+    return (
+        <section className={`w-[100%] mt-[56px]`}>
+
+            <div>
+                <Swiper
+                    breakpoints={{
+                        // When window width is >= 320px
+                        320: {
+                            slidesPerView: 3.2,
+                            spaceBetween: 20,
+                        },
+                        // When window width is >= 768px
+                        768: {
+                            slidesPerView: 5.4,
+                            spaceBetween: 30,
+                        },
+                        // When window width is >= 1024px
+                        1024: {
+                            slidesPerView: 6,
+                            spaceBetween: 40,
+                        },
+                    }}
+                    spaceBetween={5}
+                    freeMode={true}
+                    pagination={false}
+                    modules={[FreeMode, Pagination]}
+                    className="mySwiper"
+                >
                     {story.map((story: any, index: number) => (
-                        <Story key={index} {...story} />
+                        <SwiperSlide><Story key={index} {...story} /></SwiperSlide>
                     ))}
-                </section>
-            </section>
-        )
-    }
+                </Swiper>
+            </div>
+        </section >
+    )
 }
+
+
+export default Stories;
